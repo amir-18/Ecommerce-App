15c1bfadfbe6e3b977bba26353414122aa791ac3
Amir Abid <amirabid794@gmail.com>
2026-02-12 17:29:06 +0500

    Created User JOI Validation Schema

--- modules/user/userRoutes.js
+++ modules/user/userRoutes.js
@@ -2,8 +2,10 @@ import express from 'express';
 import {registerController} from './userController.js';
 const router = express.Router();
 import validation from '../../middlewares/validate.js';
-import userValidationSchema from './userValidationSchema.js';
+import {userCreateValidationSchema } from './userValidationSchema.js';
+import { userLoginValidationSchema } from './userValidationSchema.js';
 
-router.post('/register', validation(userValidationSchema),registerController);
+router.post('/register', validation(userCreateValidationSchema),registerController);
+router.post('/login', validation(userLoginValidationSchema));
 
 export default router;
