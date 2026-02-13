42d98c6c17d97b22d216cc82529d53af8bcfcbb5
Amir Abid <amirabid794@gmail.com>
2026-02-13 19:44:58 +0500

    feat : Product Creation Api is Working Successfully

--- modules/product/productController.js
+++ modules/product/productController.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { createProduct } from './productService';
+import { createProduct } from './productService.js';
 
-export const createProductController = (req,res,next) => {    
+export const createProductController = async (req,res,next) => {    
     try{
-        const product = createProduct(req.body);
+        const product = await createProduct(req.body);
         res.status(201).json({
             success : true,
             data : product,
             message : 'Product Created Successfully',
         })
+        console.log(product);
     }
     catch(error){
         next(error);
