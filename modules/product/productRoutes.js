837c06d0b995c80f5c7b82b62a32bd27af19cfbd
Amir <amirabid794@gmail.com>
2026-03-06 07:08:51 -0800

    Add README

--- modules/product/productRoutes.js
+++ modules/product/productRoutes.js
@@ -6,7 +6,7 @@ import validation from '../../middlewares/validate.js';
 const router  = express.Router();
 
 router.post('/create',Authorized,validation(productValidationSchema),createProductController);
-router.post('/products',getProductsController);
+router.get('/products',getProductsController);~
 router.post('/delete/:productid',Authorized,deleteProductController);
 router.post('/update/:productid',Authorized,updateProductController);
 export default router;
